f468f362c4239f833610a9ab66b44a961b55e23f
Aleksandra Romanova <aleksandraromanova@Aleksandras-MacBook-Pro.local>
2023-05-29 21:03:32 +0300

    Task 02 VueCalculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,44 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, reactive, watch } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+createApp({
+  setup() {
+    const data = reactive({
+      result: 0,
+      number1: 0,
+      number2: 0,
+      currentType: 'sum'
+    })
+
+    watch(
+      [() => data.number1, () => data.number2],
+      () => setResult()
+    );
+
+    const onOperatorInput = (type) => {
+      data.currentType = type;
+      setResult();
+    };
+
+    const setResult = () => {
+      switch (data.currentType) {
+        case 'sum':
+          data.result = data.number1 + data.number2;
+          break;
+        case 'subtract':
+          data.result = data.number1 - data.number2;
+          break;
+        case 'multiply':
+          data.result = data.number1 * data.number2;
+          break;
+        case 'divide':
+          data.result = data.number1 / data.number2;
+          break;
+      }
+    };
+
+    return {
+      data,
+      onOperatorInput
+    }
+  },
+}).mount('#app')
