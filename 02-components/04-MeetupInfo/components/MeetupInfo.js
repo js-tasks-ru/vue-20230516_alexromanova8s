4bb877a56178869d6f59cac1f14b699d0185ca52
Aleksandra Romanova <aleksandraromanova@Aleksandras-MacBook-Pro.local>
2023-06-07 20:24:53 +0300

    0204 task

--- 02-components/04-MeetupInfo/components/MeetupInfo.js
+++ 02-components/04-MeetupInfo/components/MeetupInfo.js
@@ -1,21 +1,45 @@
 import { defineComponent } from '../vendor/vue.esm-browser.js';
+import { format } from 'date-fns';
 
 export default defineComponent({
   name: 'MeetupInfo',
+  props: {
+    organizer: {
+      type: String,
+      required: true
+    },
+    place: {
+      type: String,
+      required: true
+    },
+    date: {
+      type: Number,
+      required: true
+    },
+  },
+
+  computed: {
+    formattedDate() {
+      return format(new Date(this.date), 'yyyy-MM-dd');
+    },
+    localeDate() {
+      return new Date(this.date).toLocaleDateString(navigator.language, { day: 'numeric', month: 'long', year: 'numeric' });
+    },
+  },
 
   template: `
     <ul class="meetup-info">
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-user.svg" />
-        Организатор
+        {{ organizer }}
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-map.svg" />
-        Место
+        {{ place }}
       </li>
       <li>
         <img class="icon meetup-info__icon" alt="icon" src="/assets/icons/icon-cal-lg.svg" />
-        <time datetime="2020-01-01">1 янв. 2020</time>
+        <time :datetime="formattedDate">{{ localeDate }}</time>
       </li>
     </ul>`,
 });
